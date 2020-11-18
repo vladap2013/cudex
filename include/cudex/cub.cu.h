4346f3c34a06e6d999629df09e9758ee33a05d35
Vladimir Petrovic <vladap2013@users.noreply.github.com>
2020-11-18 12:52:26 +0100

    CUB reduce added

--- include/cudex/cub.cu.h
+++ include/cudex/cub.cu.h
@@ -13,13 +13,11 @@ struct CubBase
 {
     CubBase(cudaStream_t stream = cudaStreamPerThread)
         : tmpMem_(0)
-        , cntMem_(1)
         , stream_(stream)
     {}
 
 protected:
     DeviceMemory<char> tmpMem_;
-    HostDeviceMemory<size_t> cntMem_;
     cudaStream_t stream_;
 };
 
@@ -32,6 +30,21 @@ struct PartitionIf : public CubBase
 
     template<typename T, typename F>
     DeviceSpan<T> runSync(DeviceSpan<T> out, DeviceSpan<const T> in, F pred);
+
+private:
+    HostDeviceMemory<size_t> cntMem_;
+};
+
+
+struct Reduce : public CubBase
+{
+    using CubBase::CubBase;
+
+    template<typename T, typename F>
+    void run(DeviceSpan<const T> data, F oper, T init, T* out);
+
+    template<typename T, typename D, typename FTransform, typename F>
+    void runTransformed(DeviceSpan<const D> data, FTransform ftransform, F oper, T init, T* out);
 };
 
 
--- include/cudex/cub.inl.cu.h
+++ include/cudex/cub.inl.cu.h
@@ -41,12 +41,31 @@ void PartitionIf::run(DeviceSpan<T> out, DeviceSpan<const T> in, Cnt* selectedCn
 template<typename T, typename F>
 DeviceSpan<T> PartitionIf::runSync(DeviceSpan<T> out, DeviceSpan<const T> in, F pred)
 {
+    cntMem_.resize(1);
+
     run(out, in, cntMem_.device().frontPtr(), std::move(pred));
     cntMem_.copyDeviceToHost();
 
     CHECK_LE(cntMem_.host()[0], in.size());
 
     return DeviceSpan<T>(out.data(), cntMem_.host()[0]);
+}
+
+template<typename T, typename F>
+void Reduce::run(DeviceSpan<const T> data, F oper, T init, T* out)
+{
+    auto fcub = cub::DeviceReduce::Reduce<const T*, T*, F, T>;
+    runCub(tmpMem_, fcub, data.data(), out, data.size(), oper, init, stream_, false);
+}
+
+template<typename T, typename D, typename FTransform, typename F>
+void Reduce::runTransformed(DeviceSpan<const D> data, FTransform ftransform, F oper, T init, T* out)
+{
+    using It = cub::TransformInputIterator<T, FTransform, const D*>;
+    It it(data.data(), ftransform);
+
+    auto fcub = cub::DeviceReduce::Reduce<It, T*, F, T>;
+    runCub(tmpMem_, fcub, it, out, data.size(), oper, init, stream_, false);
 }
 
 }
